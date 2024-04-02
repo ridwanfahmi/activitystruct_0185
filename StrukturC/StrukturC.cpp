c5423679a2c336bc319a1f27732354353ac9f3fa
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-04-02 10:53:40 +0700

    untuk menginput nomor, nama, alamat, umur mahasiswa

--- StrukturC/StrukturC.cpp
+++ StrukturC/StrukturC.cpp
@@ -10,4 +10,12 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
 }
