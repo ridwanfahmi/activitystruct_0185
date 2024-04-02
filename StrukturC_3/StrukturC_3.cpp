87a22d341734a155399f48c31c497330264b7413
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-04-02 11:58:02 +0700

    menginput alamat dan umur mahasiswa

--- StrukturC_3/StrukturC_3.cpp
+++ StrukturC_3/StrukturC_3.cpp
@@ -21,4 +21,14 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
 
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	
 }
+
